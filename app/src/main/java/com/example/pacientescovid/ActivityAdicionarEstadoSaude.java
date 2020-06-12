e765ad7a8e41f911eab8476e1441c8192a9c0c88
henrique <hicasbatatas@hotmail.com>
2020-06-12 16:29:12 +0100

    Elaboração do date picker e time picker para o registo do dia e das horas na aplicação

--- app/src/main/java/com/example/pacientescovid/ActivityAdicionarEstadoSaude.java
+++ app/src/main/java/com/example/pacientescovid/ActivityAdicionarEstadoSaude.java
@@ -2,13 +2,87 @@ package com.example.pacientescovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class ActivityAdicionarEstadoSaude extends AppCompatActivity {
+
+    Button dataButton, horasButton;
+    TextView dataTextView, horasTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_estado_saude);
+
+        dataButton = findViewById(R.id.buttonDataSaude);
+        horasButton = findViewById(R.id.buttonHoraSaude);
+        dataTextView = findViewById(R.id.textViewDiaVisita);
+        horasTextView = findViewById(R.id.textViewHoraVisita);
+
+        dataButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                testeDataButton();
+            }
+        });
+
+        horasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                testeHorasButton();
+            }
+        });
     }
+
+    private void testeDataButton(){
+
+        Calendar calendario = Calendar.getInstance();
+
+        int ano = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int date) {
+                String dataString = date + " " + month + " " + year;
+                dataTextView.setText(dataString);
+            }
+        }, ano, mes, dia);
+
+        datePickerDialog.show();
+    }
+
+    private void testeHorasButton(){
+
+        Calendar calendario = Calendar.getInstance();
+
+        int hora = calendario.get(Calendar.HOUR);
+        int minutos = calendario.get(Calendar.MINUTE);
+
+        boolean formato24Horas = DateFormat.is24HourFormat(this); //Vê o formato de horas que o utilizador tem no seu dispositivo
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                String horasString =  hour + " horas e " + minute + " minutos " ;
+                horasTextView.setText(horasString);
+            }
+        },hora, minutos, formato24Horas );// true para mostrar as horas no formato de 24h e false para formato de 12h
+
+        timePickerDialog.show();
+    }
+
+
 }
