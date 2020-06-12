96f4a5b296e2cd221263319560cd22188d1f2a3f
henrique <hicasbatatas@hotmail.com>
2020-06-12 17:02:37 +0100

    Alterações no layout

--- app/src/main/java/com/example/pacientescovid/ActivityAdicionarDoente.java
+++ app/src/main/java/com/example/pacientescovid/ActivityAdicionarDoente.java
@@ -2,13 +2,53 @@ package com.example.pacientescovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ActivityAdicionarDoente extends AppCompatActivity {
+
+    Button dataButton2;
+    TextView dataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_doente);
+
+        dataButton2 = findViewById(R.id.buttonDataNascimento);
+        dataTextView = findViewById(R.id.textViewDataNascimento);
+
+        dataButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                DataNascimentoButton();
+            }
+        });
     }
+
+    private void DataNascimentoButton(){
+
+        Calendar calendario = Calendar.getInstance();
+
+        int ano = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int date) {
+                String dataString = date + " " + month + " " + year;
+                dataTextView.setText(dataString);
+            }
+        }, ano, mes, dia);
+
+        datePickerDialog.show();
+    }
+
 }
--- app/src/main/java/com/example/pacientescovid/ActivityDoentes.java
+++ app/src/main/java/com/example/pacientescovid/ActivityDoentes.java
@@ -2,11 +2,18 @@ package com.example.pacientescovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ActivityDoentes extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +21,8 @@ public class ActivityDoentes extends AppCompatActivity {
         setContentView(R.layout.activity_doentes);
     }
 
-    public void passaAdicionarDoente (View view){
 
+    public void passaAdicionarDoente (View view){
         Intent intent = new Intent(this, ActivityAdicionarDoente.class);
         startActivity(intent);
     }
