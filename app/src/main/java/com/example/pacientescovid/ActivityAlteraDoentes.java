1c024e5ea5b07fdb9c74502f18b189806d826496
henrique <hicasbatatas@hotmail.com>
2020-06-23 17:26:19 +0100

    Layout para alterar Doentes quase concluido

---
+++ app/src/main/java/com/example/pacientescovid/ActivityAlteraDoentes.java
@@ -0,0 +1,15 @@
+package com.example.pacientescovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAlteraDoentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_altera_doentes);
+    }
+
+}
